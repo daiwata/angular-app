68070acdddd13d4b599921d4f12e2f494d1d83df
daiwata <watanabedai5427@gmail.com>
2020-03-13 00:47:24 +0900

    update CartService / and use it in product-details.component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 
 import {products } from '../products';
 
@@ -14,12 +15,18 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService,
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
     })
+  }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart");
   }
 
 }
